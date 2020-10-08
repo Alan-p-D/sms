a77b466db29d527f320e7e97b20f4cf09829c1a4
Alan Deverill <alan36pd@yandex.com>
2020-10-08 11:19:57 +0100

    Project structure
    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require_once "includes/header.php";?>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
+  <?php require_once "includes/footer.php";?>
